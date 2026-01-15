5f6e36055cc5bb28995f9abf55005fdedd0b6a22
isrhp <isrhp@proton.me>
2026-01-15 17:03:02 +0000

    Agregada vista de Ponente

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\PonenteVistaController;
+Route::get('/ponentes-vista',[PonenteVistaController::class,'index'])->name('ponentes.vista');
